7bab8822f193c84b68c24bad2abe78b95330fff6
Son Nguyen <snguyen@apptio.com>
2015-12-23 00:49:40 -0800

    Revert "Disable 'show_progress' loading indicator and up selected state for photo while it being fetched. This is to improve UX by not showing the delay after user selected a photo. The photo is technically 'selected', just not fully 'fetched' in cache yet."
    This reverts commit 6380b16818e774ec573c900893fa57c9ca44c33b.

--- src/ios/GMImagePicker/GMGridViewCell.m
+++ src/ios/GMImagePicker/GMGridViewCell.m
@@ -149,17 +149,15 @@ static UIColor *disabledColor;
 }
 
 -(void)show_progress{
-    // TODO remove this hack when reduced loading time performance
-    cell.selected = YES; // photo is selected (but not fully fetched)
-//    [self.circularProgressView setHidden:false];
+    [self.circularProgressView setHidden:false];
 }
 
 -(void)hide_progress{
-//    [self.circularProgressView setHidden:true];
+    [self.circularProgressView setHidden:true];
 }
 
 -(void)set_progress:(float)value animated:(BOOL)animated{
-//    [self.circularProgressView setProgress:value animated:animated];
+    [self.circularProgressView setProgress:value animated:animated];
 }
 
 -(void)show_fetching{
